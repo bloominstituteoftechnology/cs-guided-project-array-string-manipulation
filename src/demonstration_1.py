c77812830349a2a09c387fe6aa9fbf404a69dbf0
amichalo421 <andre.michalowski@gmail.com>
2020-12-07 14:21:51 -0500

    Demo's and reverse prob

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -1,38 +1,106 @@
-# how do we implement an array-reverse function in-place? 
-def in_place_reverse(arr):
-    # out-of-place solution 
-    # Python's slicing syntax makes a copy of the array 
-    # with the ordering of the elements reversed 
-    # Space complexity: O(n)
-    # Time complexity: O(n)
-    # return arr[::-1]
+"""
+Given an array of integers `nums`, define a function that returns the "pivot" index of the array.
+
+The "pivot" index is where the sum of all the numbers on the left of that index is equal to the sum of all the numbers on the right of that index.
+
+If the input array does not have a "pivot" index, then the function should return `-1`. If there are more than one "pivot" indexes, then you should return the left-most "pivot" index.
+
+Example 1:
+
+Input: nums = [1,7,3,6,5,6]
+Output: 3
+Explanation:
+The sum of the numbers to the left of index 3 (1 + 7 + 3 = 11) is equal to the sum of numbers to the right of index 3 (5 + 6 = 11).
+Also, 3 is the first index where this occurs.
+
+Example 2:
+
+Input: nums = [1,2,3]
+Output: -1
+Explanation:
+There is no index that satisfies the conditions in the problem statement.
+"""
+
+
+
+"""
+Given an array of integers `nums`, define a function that returns the "pivot" index of the array.
 ​
-    # how do we do this while only allocating a constant 
-    # amount of additional memory? 
-    # how can we re-use the input array? 
-    # We're going to use a two-pointer approach, starting at 
-    # the ends of the array, swapping the values those pointers 
-    # referring to, and then moving those pointers closer 
-    # towards the center of the array 
-    # Time complexity: O(n)
-    # Space: O(1)
-    left = 0 
-    right = len(arr) - 1
+The "pivot" index is where the sum of all the numbers on the left of that index is equal to the sum of all the numbers on the right of that index.
 ​
-    # we know we need to loop 
-    # what's the stopping criteria for our loop? 
-    # when the left and right pointers refer to the 
-    # same index 
-    # criteria: so long as left < right 
-    while left < right:
-        # swap the values at these two pointers 
-        arr[left], arr[right] = arr[right], arr[left]
+If the input array does not have a "pivot" index, then the function should return `-1`. If there are more than one "pivot" indexes, then you should return the left-most "pivot" index.
 ​
-        # move the pointers closer towards the center 
-        left += 1
-        right -= 1
+Example 1:
 ​
-    return arr
-    
-arr = [1,2,3,4,5,6,7,8]
-print(in_place_reverse(arr))
+Input: nums = [1,7,  3,  6,5,6]
+Output: 3       8          17
+              
+              [1,7,3,   6,   5,6]
+                11            11
+Explanation:
+The sum of the numbers to the left of index 3 (1 + 7 + 3 = 11) is equal to the sum of numbers to the right of index 3 (5 + 6 = 11).
+Also, 3 is the first index where this occurs.
+​
+Example 2:
+​
+Input: nums = [1,2, 3]
+Output: -1
+Explanation:
+There is no index that satisfies the conditions in the problem statement.
+"""
+
+
+def pivot_index(nums):
+    # Your code here
+    '''
+    Input: List of integers 
+    Output: an int, the pivot index (note that we don't count the value at the pivot)
+​
+    Plan 1: Loop through each number
+            Get the sum of all numbers to this number's left side 
+            Get the sum of all numbers to this number's right side 
+            If the two sums match, return the index of the current number 
+​
+    This plan works, but we perform a lot of redundant summing 
+    Because of the fact that we're touching every other number except the 
+    current number, for every single number in the list, this is an O(n^2) 
+    solution 
+​
+    Plan 2: keep track of the total sum of the list 
+            keep track of the current running sum as we iterate 
+            iterate through our numbers 
+            subtract the current number from the total 
+            check if the new total == running sum 
+                if it does, return the index of the current number 
+                else, add the current number to the running sum 
+            if we reach the end of the loop, then no number satisfies
+            what we're looking for, so return -1 
+​
+    For this plan, when iterating, we only take the current number and 
+    add/subtract it from a variable we're keeping track of
+    So when iterating, we only look at the current number, and none of the 
+    other numbers in the list 
+    '''
+    # O(n) + O(n) == O(2 * n) ~ O(n) time 
+    # O(1) space 
+​
+    total = sum(nums) # O(n)
+    running = 0 
+​
+    for i, num in enumerate(nums): # O(n)
+        # O(1)
+        # subtract num from total 
+        total -= num
+​
+        # check if total == running 
+        if total == running:
+            # return the index of num 
+            return i
+​
+        running += num
+​
+    return -1
+​
+​
+nums = [1,2,3]
+print(pivot_index(nums))
--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -27,82 +27,108 @@ Explanation: The input array represents the integer 999. 999 + 1 = 1000.
 """
 
 
+
+
+
 """
-Given an array of integers `nums`, define a function that returns the "pivot" index of the array.
+You are given a non-empty array that represents the digits of a non-negative integer.
 ​
-The "pivot" index is where the sum of all the numbers on the left of that index is equal to the sum of all the numbers on the right of that index.
+Write a function that increments the number by 1.
 ​
-If the input array does not have a "pivot" index, then the function should return `-1`. If there are more than one "pivot" indexes, then you should return the left-most "pivot" index.
+The digits are stored so that the digit representing the most significant place value is at the beginning of the array. Each element in the array only contains a single digit.
+​
+You will not receive a leading 0 in your input array (except for the number 0 itself).
 ​
 Example 1:
 ​
-Input: nums = [1,7,  3,  6,5,6]
-Output: 3       8          17
-              
-              [1,7,3,   6,   5,6]
-                11            11
-Explanation:
-The sum of the numbers to the left of index 3 (1 + 7 + 3 = 11) is equal to the sum of numbers to the right of index 3 (5 + 6 = 11).
-Also, 3 is the first index where this occurs.
+Input: [1,3,2]
+Output: [1,3,3]
+Explanation: The input array represents the integer 132. 132 + 1 = 133.
 ​
 Example 2:
 ​
-Input: nums = [1,2, 3]
-Output: -1
-Explanation:
-There is no index that satisfies the conditions in the problem statement.
+Input: [3,2,1,9]
+Output: [3,2,2,0]
+Explanation: The input array represents the integer 3219. 3219 + 1 = 3220.
+​
+Example 3:
+​
+Input: [9,9,9]
+Output: [1,0,0,0]
+Explanation: The input array represents the integer 999. 999 + 1 = 1000.
 """
-def pivot_index(nums):
+
+
+
+Here's the code we wrote for the plus_one problem.
+To explain a bit more why the digits.insert() call is O(n) time, this how to do with how lists are laid out in memory. For example, if we have a list such as [3, 1, 2] and we want to insert a 4 at the beginning of this list to get [4, 3, 1, 2], how is that done under the hood?
+Because of the fact that we expect that the inserted 4 will be at index 0 after the insertion, this means that the 3 has to be moved to index 1, the 1 has to be moved to index 2, and the 2 has to be moved to index 3. This all has to happen so that the slot at index 0 is available for us to park the 4 there.
+To illustrate this, here's the starting state of our list, with indices to denote where each element is:
+  0  1  2  
+[ 3, 1, 2]
+Again, we need to make space for the 4 at index 0, so in order to do that, our list is updated so that it looks like this (where the _ is denoting where the 4 will go):
+  0  1  2  3
+[ _, 3, 1, 2 ]
+Now there's space for us to park the 4 at index 0, so we end up with the final state of our list:
+  0  1  2  3
+[ 4, 3, 1, 2 ]
+The TL;DR here is that, when adding an element anywhere except the back of the list (using append / push methods), the runtime for such an operation is worst case O(n), due to the fact that we need to shift every element after the spot we want to insert into over by 1 spot. This is necessary because we have to make room for the new element we want to insert.
+
+
+
+
+def plus_one(digits):
     # Your code here
     '''
-    Input: List of integers 
-    Output: an int, the pivot index (note that we don't count the value at the pivot)
+    Input: List of integer digits 
+    Output: List of integer digits representing the original number + 1 
 ​
-    Plan 1: Loop through each number
-            Get the sum of all numbers to this number's left side 
-            Get the sum of all numbers to this number's right side 
-            If the two sums match, return the index of the current number 
+    Plan 1: Transform it into a single integer
+                - Join the list of integers into a single string 
+                - Change the single string into an integer with the `int` function
+            Add one to our transformed integer (this takes care of carrying for us)
+            Transform the integer back into a list of digits 
 ​
-    This plan works, but we perform a lot of redundant summing 
-    Because of the fact that we're touching every other number except the 
-    current number, for every single number in the list, this is an O(n^2) 
-    solution 
+    Plan 2: Can we re-use the input to do what we want to do? 
+            Can we just act on the input `digits` list?
+            [1,3,2] + 1: add 1 to the last element in the list 
+            When the last list element != 9, add 1 to it 
+            When the last list element == 9, we have to think about how 
+            to perform carry-over 
+            [9,9,9] + 1
+            
+            Recreate the process by which we would do this with pencil and paper
+            If the last element is a 9, change it to a 0, and then carry over 
+            by iterating right to left 
+            Keep iterating so long as we see 9's
+            If we get all the through the list, and we only saw 9's, then we 
+            add a 1 to the front of our list 
 ​
-    Plan 2: keep track of the total sum of the list 
-            keep track of the current running sum as we iterate 
-            iterate through our numbers 
-            subtract the current number from the total 
-            check if the new total == running sum 
-                if it does, return the index of the current number 
-                else, add the current number to the running sum 
-            if we reach the end of the loop, then no number satisfies
-            what we're looking for, so return -1 
+            Time complexity: Worst case, we're given nothing but 9's, meaning
+            we have to iterate over the entire list, O(n)
 ​
-    For this plan, when iterating, we only take the current number and 
-    add/subtract it from a variable we're keeping track of
-    So when iterating, we only look at the current number, and none of the 
-    other numbers in the list 
+            Space complexity: Since we're re-using our input list, this is 
+            constant, O(1) 
     '''
-    # O(n) + O(n) == O(2 * n) ~ O(n) time 
-    # O(1) space 
 ​
-    total = sum(nums) # O(n)
-    running = 0 
+    # iterate in reverse 
+    for i in range(len(digits) - 1, -1, -1):
+        # check if the current digit == 9 
+        if digits[i] != 9:
+            digits[i] += 1
+            # we're done; return our result 
+            return digits
 ​
-    for i, num in enumerate(nums): # O(n)
-        # O(1)
-        # subtract num from total 
-        total -= num
+        # otherwise, the current digit is 9 
+        digits[i] = 0 
+        
+    # if we reach outside of this for loop, then we got nothing but 9's 
+    # we need to add a 1 to the front of our list 
+    # Can be O(n) space in rare cases, but generally doesn't happen enough for us 
+    # to consider it 
+    # It is O(n) time complexity 
+    digits.insert(0, 1) 
 ​
-        # check if total == running 
-        if total == running:
-            # return the index of num 
-            return i
+    return digits
 ​
-        running += num
-​
-    return -1
-​
-​
-nums = [1,2,3]
-print(pivot_index(nums))
+print(plus_one([9,9])
